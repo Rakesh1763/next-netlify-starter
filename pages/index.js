21ea1651011cfb97b07aef0de5076ba579172bce
Rakesh1763 <109797375+Rakesh1763@users.noreply.github.com>
2022-07-27 18:57:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome Onboard to Vmaker Support" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <script src="https://app.getshow.io/embed/media/cbl7JYEBog8c8zwfCNZA.jsonp" async></script><div class="animaker_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="animaker_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="animaker_embed animaker_async_cbl7JYEBog8c8zwfCNZA popover=true popupDisplayType=thumbnail" style="display:inline-block;height:100%;position:relative;width:100%"><section id="Animker_player_Customization" style="height:100%;width:100%;"></section></div></div></div><script src="https://static.getshow.io/play/app.bundle.js" type="text/javascript" async></script>
         </p>
       </main>
 
